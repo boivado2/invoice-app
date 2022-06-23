8f6b573813142b34866adf30a63df4ad602ae879
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-06-23 03:52:03 +0100

    deleted the  routing implementation from app.js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, {useEffect} from 'react';
 import { useSelector } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { getFilterByStatus } from './app/invoices';
 import Home from './components/Home';
-import InvoiceDetail from './components/InvoiceDetail';
 import Navbar from './components/Navbar';
 
 
@@ -27,15 +24,9 @@ function App() {
   return (
     <div className={` ${invoiceForm ? 'fixed ': ''} flex flex-1 flex-col lg:flex-row dark:bg-custom-dark-blue-300 bg-custom-ligth-300 w-full h-full min-h-screen font-spartan`} >
       <Navbar/>
-      <Router>
-        <Routes>
-          <Route path='/'>
-            <Route  index element={<Home />} />
-            <Route path=':id' element={<InvoiceDetail />} />
-          </Route>
-        </Routes>
-    </Router>
-</div>
+      <Home />
+
+    </div>
   );
 }
 
