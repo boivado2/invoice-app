9d4530594c04d6591a708415233576fc30e0e742
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-06-26 14:17:18 +0100

    fix the text color of the emplty invoice page to support dark mode .

--- src/components/NoInvoice.jsx
+++ src/components/NoInvoice.jsx
@@ -3,7 +3,7 @@ import noInvoiceSvg from '../assets/illustration-empty.svg'
 
 function NoInvoice() {
   return (
-    <aside className='flex flex-col items-center justify-center py-8 px-4 h-full  2xl:h-[500px]  3xl:h-[800px] w-full'>
+    <aside className='flex flex-col items-center justify-center py-8 px-4 h-full  2xl:h-[500px]  3xl:h-[800px] w-full dark:text-custom-ligth-100 text-custom-dark-blue-400'>
       <img className='md:h-[250px] lg:h-[280px] 3xl:h-[600px]' src={noInvoiceSvg} alt="" />
       <h2 className='text-xl md:text-3xl 2xl:text-4xl font-medium mt-12 mb-3'>There is nothing here</h2>
       <p className='text-md md:text-xl 2xl:text-2xl text-center'>
