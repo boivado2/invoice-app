c339bee9bb210c65b6325a07d3ceacb64c689499
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-08-11 23:01:25 +0100

    fix the layout issue displaying error msg in select component and background color.

--- src/components/common/Select.jsx
+++ src/components/common/Select.jsx
@@ -16,7 +16,7 @@ function Select({ items, name, label, styles }) {
         <ErrorMessage style={`hidden md:block text-custom-ligth-red-100`} visible={touched[name]} error={errors[name]} />
       </div>
       
-      <select  value={values[name]} onChange={(e) => setFieldValue(name, e.target.value) } name={name} onBlur={() => setFieldTouched(name)} className={` px-9 py-4 w-full bg-custom-color-white-100 outline-none ${styles}`} >
+      <select  value={values[name]} onChange={(e) => setFieldValue(name, e.target.value) } name={name} onBlur={() => setFieldTouched(name)} className={` px-9 py-4 w-full  dark:bg-custom-dark-blue-100 bg-custom-ligth-100 dark:text-custom-ligth-200  outline  outline-[0.3px] focus:dark:outline-custom-dark-purple focus:outline-custom-dark-purple dark:outline-custom-dark-blue-300  text-base ${errors[name] ? ' dark:outline-custom-ligth-red-100 outline-custom-ligth-red-100' :  "outline-custom-ligth-200"} ${styles}`} >
       <option className='w-full' value="" >select</option>
         {items.map(item => (
             <option className='w-full' key={item.paymentTerms} value={item.paymentTerms}>{item.title}</option>
